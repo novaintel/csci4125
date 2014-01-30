e4feda85695a366a3d890232d2a04e493636efb3
James Hill <jaed.llih@gmail.com>
2014-01-30 16:04:36 -0400

    Switch over to Q1 of A1

---
+++ Assignment1/Assignment1/gameOfLife.cpp
@@ -0,0 +1,201 @@
+/***********************
+
+Conway Game of Life
+
+************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void lifeGame(int size, int gens, string *rs){
+
+	int i, j, n, im, ip, jm, jp, ni, nj, nsum, isum;
+	int **oldd, **newd;
+	float x;
+	FILE *outFile;
+
+	clock_t start;
+	clock_t finish;
+	string ptime = "";
+	string intm = "";
+	double interval = 0;
+
+
+	/* allocate arrays */
+	start = clock();
+
+	ni = size + 2;  /* add 2 for left and right ghost cells */
+	nj = size + 2;
+	oldd = (int **) malloc(ni*sizeof(int*));
+	newd = (int **) malloc(ni*sizeof(int*));
+
+	for (i = 0; i<ni; i++){
+		oldd[i] = (int *) malloc(nj*sizeof(int));
+		newd[i] = (int *) malloc(nj*sizeof(int));
+	}
+
+
+	finish = clock() - start;
+	interval = finish / (double) CLOCKS_PER_SEC;
+	intm = to_string(interval);
+	*rs = *rs + "," + intm;
+
+
+	/*  initialize elements of old to 0 or 1 */
+	start = clock();
+
+	for (i = 1; i <= size; i++){
+		for (j = 1; j <= size; j++){
+			x = rand() / ((float) RAND_MAX + 1);
+			if (x<0.5){
+				oldd[i][j] = 0;
+			}
+			else {
+				oldd[i][j] = 1;
+			}
+		}
+	}
+
+	finish = clock() - start;
+	interval = finish / (double) CLOCKS_PER_SEC;
+	intm = to_string(interval);
+	*rs = *rs + "," + intm;
+
+
+	/*  time steps */
+	start = clock();
+
+	for (n = 0; n<gens; n++){
+
+		/* corner boundary conditions */
+		oldd[0][0] = oldd[size][size];
+		oldd[0][size + 1] = oldd[size][1];
+		oldd[size + 1][size + 1] = oldd[1][1];
+		oldd[size + 1][0] = oldd[1][size];
+
+		/* left-right boundary conditions */
+
+		for (i = 1; i <= size; i++){
+			oldd[i][0] = oldd[i][size];
+			oldd[i][size + 1] = oldd[i][1];
+		}
+
+		/* top-bottom boundary conditions */
+
+		for (j = 1; j <= size; j++){
+			oldd[0][j] = oldd[size][j];
+			oldd[size + 1][j] = oldd[1][j];
+		}
+
+		for (i = 1; i <= size; i++){
+			for (j = 1; j <= size; j++){
+				im = i - 1;
+				ip = i + 1;
+				jm = j - 1;
+				jp = j + 1;
+
+				nsum = oldd[im][jp] + oldd[i][jp] + oldd[ip][jp]
+					+ oldd[im][j] + oldd[ip][j]
+					+ oldd[im][jm] + oldd[i][jm] + oldd[ip][jm];
+
+				switch (nsum){
+
+				case 3:
+					newd[i][j] = 1;
+					break;
+
+				case 2:
+					newd[i][j] = oldd[i][j];
+					break;
+
+				default:
+					newd[i][j] = 0;
+				}
+			}
+		}
+
+		/* copy new state into old state */
+
+		for (i = 1; i <= size; i++){
+			for (j = 1; j <= size; j++){
+				oldd[i][j] = newd[i][j];
+			}
+		}
+	}
+
+	finish = clock() - start;
+	interval = finish / (double) CLOCKS_PER_SEC;
+	intm = to_string(interval);
+	*rs = *rs + "," + intm;
+
+
+	/*  Iterations are done; sum the number of live cells */
+	start = clock();
+
+	isum = 0;
+	for (i = 1; i <= size; i++){
+		for (j = 1; j <= size; j++){
+			isum = isum + newd[i][j];
+		}
+	}
+
+	finish = clock() - start;
+	interval = finish / (double) CLOCKS_PER_SEC;
+	intm = to_string(interval);
+	*rs = *rs + "," + intm;
+
+
+	printf("\nNumber of live cells = %d\n", isum);
+
+	return;
+}
+
+int TimingF(){
+	int bdsize = 200;
+	int gens = 500;
+	int rounds = 10;
+	string file = "";
+
+	cout << "Please input board size:";
+	cin >> bdsize;
+	cout << endl;
+
+	cout << "Please input number of generations:";
+	cin >> gens;
+	cout << endl;
+
+	cout << "Please input number of rounds:";
+	cin >> rounds;
+	cout << endl;
+
+	cout << "Please input file to store result:";
+	getline(cin, file);
+	getline(cin, file);
+	cout << endl;
+
+	fstream fs;
+	fs.open(file, fstream::in | fstream::out | fstream::app);
+	fs << "\n";
+
+	for (int i = 0; i < rounds; i++){
+		string re = "";
+		re = "S:" + to_string(bdsize) + "G:" + to_string(gens) + "R:" + to_string(i);
+		lifeGame(bdsize, gens, &re);
+		re = re + "\n";
+		fs << re;
+	}
+
+	fs.close();
+
+	return 0;
+}
+
+int main(int argc, char *argv[]){
+	return TimingF();
+}
--- Assignment1/Assignment1/lifeGame_MPI.c
+++ Assignment1/Assignment1/lifeGame_MPI.c
@@ -14,33 +14,69 @@ void printLiveCell(int **board, int boardLength){
 	printf("\nNumber of live cells = %d\n", isum);
 }
 
-void randomGame(int** matrix, int** temp, int matrix_size, int mysize){
-	/* Initialize the boundaries of the life matrix */
+void createRandomBoard(int **board, int **tempBoard, int boardSideLength, int threadBoardSize){
 	int i, j, x;
-	for (j = 0; j < matrix_size + 2; j++)
-		matrix[0][j] = matrix[mysize + 1][j] = temp[0][j] = temp[mysize + 1][j] = 0;
-	for (i = 0; i < mysize + 2; i++)
-		matrix[i][0] = matrix[i][matrix_size + 1] = temp[i][0] = temp[i][matrix_size + 1] = 0;
+
+	/* Initialize the boundaries of the life matrix */
+	for (j = 0; j < boardSideLength + 2; j++)
+		board[0][j] = board[threadBoardSize + 1][j] = tempBoard[0][j] = tempBoard[threadBoardSize + 1][j] = 0;
+	for (i = 0; i < threadBoardSize + 2; i++)
+		board[i][0] = board[i][boardSideLength + 1] = tempBoard[i][0] = tempBoard[i][boardSideLength + 1] = 0;
 
 	/* Initialize the life matrix */
-	for (i = 1; i <= mysize; i++)  {
-		for (j = 1; j <= matrix_size; j++)
+	for (i = 1; i <= threadBoardSize; i++)  {
+
+		for (j = 1; j <= boardSideLength; j++){
 			x = rand() / ((float) RAND_MAX + 1);
-		if (x<0.5){
-			matrix[i][j] = 0;
-		}
-		else {
-			matrix[i][j] = 1;
+			if (x < 0.5){
+				board[i][j] = 0;
+			}
+			else {
+				board[i][j] = 1;
+			}
 		}
 	}
 }
 
+void runRoundOfLife(int **board, int **tempBoard, int boardSideLength, int threadBoardSize, int previousThreadRank, int nextThreadRank, MPI_Comm comm){
+	int i, j, sum;
 
-void playRoundOfLife(int numCycles, int boardSideLength, MPI_Comm comm){
+	MPI_Request      req[4];
+	MPI_Status       status[4];
+
+	MPI_Isend(&board[1][0], boardSideLength + 2, MPI_INT, previousThreadRank, 0, comm, req);
+	MPI_Irecv(&board[0][0], boardSideLength + 2, MPI_INT, previousThreadRank, 0, comm, req + 1);
+	MPI_Isend(&board[threadBoardSize][0], boardSideLength + 2, MPI_INT, nextThreadRank, 0, comm, req + 2);
+	MPI_Irecv(&board[threadBoardSize + 1][0], boardSideLength + 2, MPI_INT, nextThreadRank, 0, comm, req + 3);
+	MPI_Waitall(4, req, status);
+
+
+	for (i = 1; i <= threadBoardSize; i++) {
+		for (j = 1; j < boardSideLength + 1; j++) {
+
+
+			sum = board[i - 1][j - 1] + board[i - 1][j] + board[i - 1][j + 1]
+				+ board[i][j - 1] + board[i][j + 1]
+				+ board[i + 1][j - 1] + board[i + 1][j] + board[i + 1][j + 1];
+
+			if (sum < 2 || sum > 3)
+				tempBoard[i][j] = 0;
+			else if (sum == 3)
+				tempBoard[i][j] = 1;
+			else
+				tempBoard[i][j] = board[i][j];
+		}
+	}
+
+	board = tempBoard;
+}
+
+void playGameOfLife(int numCycles, int boardSideLength, MPI_Comm comm){
+
 
 	int **board, **tempBoard;
 
-	int i, j, k;
+	int i, j, k, x;
 
 	int currentThreadRank, previousThreadRank, nextThreadRank, size;
 	int threadBoardSize, threadOffset;
@@ -81,112 +117,34 @@ void playRoundOfLife(int numCycles, int boardSideLength, MPI_Comm comm){
 		tempBoard[i] = (int *) malloc(sizeof(int) *(threadBoardSize + 2));
 	}
 
-
-	randomGame(board, tempBoard, boardSideLength, threadBoardSize);
+	createRandomBoard(board, tempBoard, boardSideLength, threadBoardSize);
 
 	for (k = 0; k < numCycles; k++) {
-		MPI_Request      req[4];
-		MPI_Status       status[4];
-
-		/* Send and receive boundary information */
-		MPI_Isend(&board[1][0], boardSideLength + 2, MPI_INT, previousThreadRank, 0, comm, req);
-		MPI_Irecv(&board[0][0], boardSideLength + 2, MPI_INT, previousThreadRank, 0, comm, req + 1);
-		MPI_Isend(&board[threadBoardSize][0], boardSideLength + 2, MPI_INT, nextThreadRank, 0, comm, req + 2);
-		MPI_Irecv(&board[threadBoardSize + 1][0], boardSideLength + 2, MPI_INT, nextThreadRank, 0, comm, req + 3);
-		MPI_Waitall(4, req, status);
-
-		/* For each element of the matrix ... */
-		for (i = 1; i <= threadBoardSize; i++) {
-			for (j = 1; j < boardSideLength + 1; j++) {
-
-				/* find out the value of the current cell */
-				sum = board[i - 1][j - 1] + board[i - 1][j] + board[i - 1][j + 1]
-					+ board[i][j - 1] + board[i][j + 1]
-					+ board[i + 1][j - 1] + board[i + 1][j] + board[i + 1][j + 1];
-
-				/* check if the cell dies or life is born */
-				if (sum < 2 || sum > 3)
-					tempBoard[i][j] = 0;
-				else if (sum == 3)
-					tempBoard[i][j] = 1;
-				else
-					tempBoard[i][j] = board[i][j];
-			}
-		}
-		/* Swap the matrices */
-		board = tempBoard;
+		runRoundOfLife(board, tempBoard, boardSideLength, threadBoardSize, previousThreadRank, nextThreadRank, comm);
 	}
-
+	
 	printLiveCell(board, boardSideLength);
 }
 
 
-int main(int argc, char* argv[]){
-	int my_rank;
-	int p;
-	int source;
-	int dest;
-	int tag = 0;
-	char message[100];
-	int boardSideLength;
-	int iters;
-	MPI_Status status;
-
+int main(argc, argv)
+int argc;
+char *argv[];
+{
+	int rank, N, iters;
+	double time;
 	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+	N = 1000;
+	iters = 500;
 
-	MPI_Comm_size(MPI_COMM_WORLD, &p);
-
-
-	if (my_rank == 0) {
-		printf("Matrix Size : ");
-		scanf("%d", &boardSideLength);
-		printf("Iterations : ");
-		scanf("%d", &iters);
-	}
-
-	MPI_Bcast(&boardSideLength, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	MPI_Bcast(&iters, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-	playRoundOfLife(iters, boardSideLength, MPI_COMM_WORLD);
+	playGameOfLife(N, iters, MPI_COMM_WORLD);
 
 	MPI_Finalize();
 
 }
 
-
-/*
-#include <stdio.h>
-#include <string.h>
-#include "mpi.h"
-
-int main(int argc, char* argv[]){
-int my_rank;
-int p;
-int source;
-int dest;
-int tag = 0;
-char message[100];
-MPI_Status status;
-
-MPI_Init(&argc, &argv);
-
-MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-
-MPI_Comm_size(MPI_COMM_WORLD, &p);
-
-if (my_rank != 0){
-sprintf(message, "Greetings from process %d!", my_rank);
-dest = 0;
-MPI_Send(message, strlen(message) + 1, MPI_CHAR, dest, tag, MPI_COMM_WORLD);
-
-}
-else {
-for (source = 1; source < p; source++){
-MPI_Recv(message, 100, MPI_CHAR, source, tag, MPI_COMM_WORLD, &status);
-printf("%s\n", message);
-}
-}
-MPI_Finalize();
-}*/
